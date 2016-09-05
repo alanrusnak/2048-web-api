0a6800a0bb0b6e2d9bbb0971b221e8da5cebcf91
alanrusnak <alanrusnak9@gmail.com>
2016-09-05 22:02:22 +0100

    jQuery communicate via Ajax and display board

--- src/main/resources/static/scripts/2048.js
+++ src/main/resources/static/scripts/2048.js
@@ -1,10 +1,33 @@
+var gameId;
+
 function newGame() {
    $.ajax({
            url: "/2048/new"
        }).then(function(data) {
+          gameId = data.id;
           $('.game-id').append(data.id);
           $('.game-score').append(data.score);
-          $('.game-board').append(data.board.tiles);
+          $('#tile-row-one').text(data.board.tiles[0]);
+          $('#tile-row-two').text(data.board.tiles[1]);
+          $('#tile-row-three').text(data.board.tiles[2]);
+          $('#tile-row-four').text(data.board.tiles[3]);
           console.log(data);
+       });
+};
+
+function displayData(data){
+$('.game-score').text("Score: " + data.score);
+          $('#tile-row-one').text(data.board.tiles[0]);
+          $('#tile-row-two').text(data.board.tiles[1]);
+          $('#tile-row-three').text(data.board.tiles[2]);
+          $('#tile-row-four').text(data.board.tiles[3]);
+          console.log(data);
+}
+
+function move(direction) {
+   $.ajax({
+           url: "/2048/" + gameId + "/" + direction
+       }).then(function(data) {
+          displayData(data);
        });
 };
