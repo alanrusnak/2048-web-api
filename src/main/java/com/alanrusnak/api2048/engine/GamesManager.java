a294b23cf2d3f7b028e7fac02dc2d1bd751a25c9
alanrusnak <alanrusnak9@gmail.com>
2016-09-10 15:13:12 +0100

    Expire games which havent moved for an hour

--- src/main/java/com/alanrusnak/api2048/engine/GamesManager.java
+++ src/main/java/com/alanrusnak/api2048/engine/GamesManager.java
@@ -6,8 +6,15 @@ import com.alanrusnak.api2048.engine.model.MoveResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class GamesManager {
@@ -21,7 +28,30 @@ public class GamesManager {
     @Autowired
     private GameOverDetector gameOverDetector;
 
-    private Map<String, Game> activeGames = new HashMap<String, Game>();
+    private Map<String, Game> activeGames = new ConcurrentHashMap<String, Game>();
+    private final int gameExpiryTimeInHours = 1;
+
+    @PostConstruct
+    public void init(){
+        final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                removeInactiveGames();
+            }
+        };
+        scheduler.scheduleAtFixedRate(runnable, gameExpiryTimeInHours, gameExpiryTimeInHours, TimeUnit.HOURS);
+    }
+
+    private void removeInactiveGames(){
+        System.out.println("Removing inactive games. Current number of games: " + activeGames.size());
+        for (Game game : activeGames.values()) {
+            if(game.getLastMoveTime().isBefore(LocalDateTime.now().minusHours(gameExpiryTimeInHours))){
+                activeGames.remove(game.getId());
+            }
+        }
+        System.out.println("Removed inactive games. Number of games after removal: " + activeGames.size());
+    }
 
     public Game createNewGame() {
         String id = randomStateGenerator.generateGameId();
@@ -37,6 +67,10 @@ public class GamesManager {
 
     public Game move(String id, String direction){
         Game game = activeGames.get(id);
+        if(game == null){
+            return null;
+        }
+
         if(game.isGameOver()){
             return game;
         }
@@ -44,6 +78,7 @@ public class GamesManager {
         MoveResult result = moveExecutor.move(board, Integer.parseInt(direction));
         if(result.wasTileMoved()){
             game.addScore(result.getScore());
+            game.setLastMoveTimeToNow();
             randomStateGenerator.generateNextRandomTile(board);
             game.setGameOver(gameOverDetector.isGameOver(board));
         }
--- src/main/java/com/alanrusnak/api2048/engine/model/Game.java
+++ src/main/java/com/alanrusnak/api2048/engine/model/Game.java
@@ -1,17 +1,25 @@
 package com.alanrusnak.api2048.engine.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.time.LocalDateTime;
+
 public class Game {
 
     private String id;
     private long score;
     private Board board;
     private boolean isGameOver;
+    private LocalDateTime startTime;
+    private LocalDateTime lastMoveTime;
 
     public Game(String id, Board board) {
         this.id = id;
         this.score = 0;
         this.board = board;
-        isGameOver = false;
+        this.isGameOver = false;
+        this.startTime = LocalDateTime.now();
+        this.lastMoveTime = LocalDateTime.now();
     }
 
     public String getId() {
@@ -36,5 +44,19 @@ public class Game {
 
     public void setGameOver(boolean gameOver) {
         isGameOver = gameOver;
+    }
+
+    @JsonIgnore
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    @JsonIgnore
+    public LocalDateTime getLastMoveTime() {
+        return lastMoveTime;
+    }
+
+    public void setLastMoveTimeToNow(){
+        this.lastMoveTime = LocalDateTime.now();
     }
 }
